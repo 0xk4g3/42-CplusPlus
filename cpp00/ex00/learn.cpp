f268b4f7abc0e9612599508acd4eacab7223cbde
0xk4g3 <hbendjab@student.42abudhabi.ae>
2024-12-27 00:10:41 +0400

    Finall submision of ex00
    i pushed the final version of the cpp00 ex00

---
+++ cpp00/ex00/learn.cpp
@@ -0,0 +1,17 @@
+
+
+
+#include <iostream>
+#include <string>
+
+
+
+int main(){
+
+    
+    std::string text ="haithem";
+
+    std::string::iterator it != text.end() ;   
+
+    std::cout << *it << std::endl;
+}
--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -1,4 +1,5 @@
 
+
 #include <iostream>
 #include <string>
 #include  <cctype>
@@ -6,29 +7,30 @@
 
 
 
-void toUpper(std::string &str){
-    
-    
-    for (size_t i = 0; i  < str.size() ; i++) {
-        str[i] = std::toupper(static_cast<unsigned char>(str[i])); 
-    }
-
+void   ChangeToUpperCase(std::string &UserInput){
+        
+         
+         for(std::string::iterator character = UserInput.begin() ;character != UserInput.end() ; character++){
+               if(*character >= 'a'  &&  *character <=  'z')
+                        *character -= 32; 
+         }
 }
 
 
 int main(int ac , char **av ){
     
  
-    if(ac  == 1)
+    if(ac  == 1){
         std::cout<< "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+            return 0;
+    }
      
-    
-     for (int i = 1; i < ac  ;  i++) {
-             
-             std::string str = av[i];
-            toUpper(str); 
-            std::cout << str ;
-     }
-     std::cout  << std::endl;
-    
+    for(int  i =  1 ; i < ac ; i++){
+         
+         std::string UserText = av[i];
+         ChangeToUpperCase(UserText);
+         std::cout << UserText ;   
+    }
+    std::cout << std::endl;
+    return 0;
 }
