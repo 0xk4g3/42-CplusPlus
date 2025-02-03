2a4e3127022d4e7a8f1c37ec53b7ea485de638eb
0xk4g3 <hbendjab@student.42abudhabi.ae>
2025-02-03 15:34:05 +0400

    add the  init files for the cpp02

---
+++ cpp02/ex00/Fixed.cpp
@@ -0,0 +1,19 @@
+
+
+
+#include <cstdio>
+
+
+
+
+
+int main(){
+     
+
+     if((0.1 + 0.2) == 0.3)
+       printf("it's true yeah equal \n");
+    else
+     printf("NOT equal\n");
+
+    return 0;
+}
---
+++ cpp02/ex00/Fixed.s
@@ -0,0 +1,28 @@
+	.file	"Fixed.cpp"
+	.text
+	.section	.rodata
+.LC0:
+	.string	"NOT equal" 
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB3:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	leaq	.LC0(%rip), %rax
+	movq	%rax, %rdi
+	call	puts@PLT
+	movl	$0, %eax
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE3:
+	.size	main, .-main
+	.ident	"GCC: (Debian 14.2.0-8) 14.2.0"
+	.section	.note.GNU-stack,"",@progbits
